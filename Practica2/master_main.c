6ed38f50d937238514b09d93643c86f99bf209da
Axel Torres <axlalet@icloud.com>
2024-11-06 15:15:46 -0600

    compilacion

--- Practica2/master_main.c
+++ Practica2/master_main.c
@@ -4,13 +4,13 @@
 #define _XTAL_FREQ 4000000
 
 void main(void) {
-    unsigned char status;
+    unsigned char status,data;
 
     I2C_Master_Init(); //Se debe inicializar siempre
 
     while(1) {
         //usar I2C_Master_Send cada que se quiera comunicar un dato
-        status = I2C_Master_Send(test_data[index]);
+        status = I2C_Master_Send(data);
         if (status != 0) {
             //llevar a cabo acciones en caso de error
             //1 y 2 son codigos de error
